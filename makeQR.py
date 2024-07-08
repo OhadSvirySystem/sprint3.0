993f97e1333721b99a3dd5cf6694b5f66182a0fb
geverMeod <tamaromrinit@gmail.com>
2024-07-08 17:31:07 +0300

    chore: Refactor makeQR.py to improve QR code display and remove unnecessary code

--- makeQR.py
+++ makeQR.py
@@ -16,26 +16,15 @@ def generate_qr_code(text, index):
     img.save(f'qr_code_{index}.png')
 
 def display_qr_codes(text_list):
-    screen_res = (1920, 1080)  # Set this to your screen resolution
     for index, text in enumerate(text_list):
         generate_qr_code(text, index)
-        qr_img = cv2.imread(f'qr_code_{index}.png')
+        img = cv2.imread(f'qr_code_{index}.png')
 
         # Create a fullscreen window
         cv2.namedWindow('QR Code', cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty('QR Code', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-
-        # Create a black background image
-        background = np.zeros((screen_res[1], screen_res[0], 3), dtype=np.uint8)
-
-        # Calculate the position to center the QR code
-        y_offset = (screen_res[1] - qr_img.shape[0]) // 2
-        x_offset = (screen_res[0] - qr_img.shape[1]) // 2
-
-        # Place the QR code on the background
-        background[y_offset:y_offset + qr_img.shape[0], x_offset:x_offset + qr_img.shape[1]] = qr_img
-
-        cv2.imshow('QR Code', background)
+        
+        cv2.imshow('QR Code', img)
         cv2.waitKey(1000)  # Display each QR code for 1 second
 
     cv2.destroyAllWindows()
@@ -53,12 +42,27 @@ def read_string_from_file(file_path):
     except FileNotFoundError:
         print(f"Error: The file {file_path} was not found.")
         return None
+def display_red_screen():
+    """
+    Display a red screen using OpenCV.
+    """
+    # Create a red screen
+    red_screen = np.zeros((1080, 1920, 3), np.uint8)
+    red_screen[:] = (0, 0, 255)
 
+    # Create a fullscreen window
+    cv2.namedWindow('Red Screen', cv2.WND_PROP_FULLSCREEN)
+    cv2.setWindowProperty('Red Screen', cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+    cv2.imshow('Red Screen', red_screen)
+    cv2.waitKey(3000)  # Display the red screen for 3 seconds
+
+    cv2.destroyAllWindows()
 if __name__ == "__main__":
-    charsInQR = 200 
-    text = read_string_from_file("The Little Prince.txt")
-    text = text[:600]
-    text = " "*charsInQR*4 + text
+    charsInQR = 100 
+    text = read_string_from_file("omri1.txt")
     if text:
+        text = " " * 2 * charsInQR + text
         text_list = [text[i:i+charsInQR] for i in range(0, len(text), charsInQR)]
+        display_red_screen()
         display_qr_codes(text_list)
